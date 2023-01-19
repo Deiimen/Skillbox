0bfd53d075e51d30c4b46acd04ec4f78dd7228bb
Dmitry Imanakov <angels.night@mail.ru>
2023-01-19 17:26:07 +0530

    second change

--- 1.py
+++ 1.py
@@ -1,10 +1,11 @@
 def test():
     n = int(input('Введите число: '))
     summ = 1
-    for i in range(1,50+1):
+    for i in range(1, 50+1):
         if i % n == 0:
             print(i, end=' ')
             summ += i
     print('\n\nСумма равна:', summ)
+
 
 test()
