ae5f5c99e798a9e8cb889bbc5866ce1a0396445f
Deiimen <122734667+Deiimen@users.noreply.github.com>
2023-01-19 17:35:59 +0530

    Change print

--- 1.py
+++ 1.py
@@ -1,9 +1,10 @@
 def test():
     n = int(input('Введите число: '))
     summ = 1
+    print('числа ряда кратных',n, end=' ')
     for i in range(1, 50+1):
         if i % n == 0:
-            print('числа ряда',i, end=' ')
+            print(i, end=' ')
             summ += i
     print('\n\nСумма равна:', summ)
 
